0ccca01cbc335482b2b0a7165e5d4db1d74fc388
Mahmoud Safan <mahmoudsafan240@gmail.com>
2022-04-20 08:46:50 +0200

    List Implementation

--- List.js
+++ List.js
@@ -16,9 +16,8 @@
 √ next (function)             --> Moves current position forward one element
 √ currPos (function)          --> Returns the current position in list
 √ moveTo (function)           --> Moves the current position to specified position
+√ isEmpty (function)          --> Returns true/false
 */
-
-
 
 
 class List{
